6bb6a88b8a98671715fa7634b6644833e09e7e95
Laise France <laise94@hotmail.com>
2023-04-15 18:08:46 -0300

    ajusta letras e especiais

--- criptografia.js
+++ criptografia.js
@@ -1,43 +1,78 @@
-function criptografar(texto){
-    // converter para letras minúsculas
-    texto = texto.toLowerCase();
-    
-    // substituir as letras
-    texto = texto.replace(/a/g, 'ai');
-    texto = texto.replace(/e/g, 'enter');
-    texto = texto.replace(/i/g, 'imes');
-    texto = texto.replace(/o/g, 'ober');
-    texto = texto.replace(/u/g, 'ufat');
-    
-    // retornar a versão criptografada
-    return texto;
+// Bloquear caracteres especiais e letras maiúsculas
+const textoInput = document.querySelector("#texto-input");
+
+textoInput.addEventListener("keydown", function(e) {
+  if (!checkChar(e)) {
+    e.preventDefault();
+  }
+});
+
+function checkChar(e) {
+  // nao aceitar caracteres especiais e letras maiúsculas e emite um alerta
+  const charCode = e.keyCode;
+
+  if (charCode >= 65 && charCode <= 90) {
+    alert("Não é permitido letras maiúsculas!");
+    return false;
+  } else if (charCode >= 32 && charCode <= 47) {
+    alert("Não é permitido caracteres especiais!");
+    return false;
+  } else {
+    return true;
+  }
 }
 
-function descriptografar(texto){
-    texto = texto.toLowerCase();
-    // substituir as letras
-    texto = texto.replace(/ai/g, 'a');
-    texto = texto.replace(/enter/g, 'e');
-    texto = texto.replace(/imes/g, 'i');
-    texto = texto.replace(/ober/g, 'o');
-    texto = texto.replace(/ufat/g, 'u');
-    
-    // retornar a versão original
-    return texto;
+//console.log(checkChar('a'));
+//console.log(checkChar('A'));
+
+function criptografar(texto) {
+  // substituir as letras
+  texto = texto.replace(/a/g, "ai");
+  texto = texto.replace(/e/g, "enter");
+  texto = texto.replace(/i/g, "imes");
+  texto = texto.replace(/o/g, "ober");
+  texto = texto.replace(/u/g, "ufat");
+
+  if (checkChar(texto)) {
+    console.log(texto);
+  } else {
+    alert("Não é permitido caracteres especiais e letras maiúsculas!");
+  }
+
+  // retornar a versão criptografada
+
+  return texto;
 }
 
-function copiarTexto(){ 
-    var texto = document.getElementById('resultado').innerHTML;
-    copiarValorCampo(texto);
-    alert('Texto copiado para a área de transferência!');
-} 
+function descriptografar(texto) {
+  // substituir as letras
+  texto = texto.replace(/ai/g, "a");
+  texto = texto.replace(/enter/g, "e");
+  texto = texto.replace(/imes/g, "i");
+  texto = texto.replace(/ober/g, "o");
+  texto = texto.replace(/ufat/g, "u");
+  if (checkChar(texto)) {
+    console.log(texto);
+  } else {
+    alert("Não é permitido caracteres especiais e letras maiúsculas!");
+  }
+
+  // retornar a versão descriptografada
+  return texto;
+}
+
+function copiarTexto() {
+  var texto = document.getElementById("resultado").innerHTML;
+  copiarValorCampo(texto);
+  alert("Texto copiado para a área de transferência!");
+}
 
 function copiarValorCampo(valorCampo) {
-    var inputTemp = document.createElement("input");
-    inputTemp.style = "position: absolute; left: -1000px; top: -1000px";
-    inputTemp.value = valorCampo;
-    document.body.appendChild(inputTemp);
-    inputTemp.select();
-    document.execCommand("copy");
-    document.body.removeChild(inputTemp);
+  var inputTemp = document.createElement("input");
+  inputTemp.style = "position: absolute; left: -1000px; top: -1000px";
+  inputTemp.value = valorCampo;
+  document.body.appendChild(inputTemp);
+  inputTemp.select();
+  document.execCommand("copy");
+  document.body.removeChild(inputTemp);
 }
