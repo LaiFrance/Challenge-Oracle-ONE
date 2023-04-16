e830ef99342440f9ee645827a617c66cb36985b3
Laise France <laise94@hotmail.com>
2023-04-16 10:09:15 -0300

    alterações

--- criptografia.js
+++ criptografia.js
@@ -1,28 +1,39 @@
 // Bloquear caracteres especiais e letras maiúsculas
-
-textoInput.addEventListener("keydown", function(e) {
-  if (!checkChar(e)) {
-    e.preventDefault();
-  }
-});
+//let texto = document.querySelector('#texto')
+function Init() {
+  var textoA = document.getElementById("texto");
+  textoA.addEventListener("keypress", checkChar);
+}
 
 function checkChar(e) {
   // nao aceitar caracteres especiais e letras maiúsculas e emite um alerta
-  const charCode = e.keyCode;
+  var charCode = e.keyCode;
 
   if (charCode >= 65 && charCode <= 90) {
     alert("Não é permitido letras maiúsculas!");
+    e.preventDefault();
     return false;
+    // Caracteres especiais
   } else if (charCode >= 32 && charCode <= 47) {
     alert("Não é permitido caracteres especiais!");
+    e.preventDefault();
+    // numeros
+  } else if (charCode >= 48 && charCode <= 57) {
+    alert("Não é permitido!");
+    e.preventDefault();
+    // ACENTOS
+  } else if (charCode >= 192 && charCode <= 255) {
+    alert("Não é permitido !");
+    e.preventDefault();
+    // não aceita @ nem ç nem ~
+  } else if (charCode == 64 || charCode == 231 || charCode == 126) {
+    alert("Não é permitido caracteres especiais!");
+    e.preventDefault();
     return false;
   } else {
     return true;
   }
 }
-
-//console.log(checkChar('a'));
-//console.log(checkChar('A'));
 
 function criptografar(texto) {
   // substituir as letras
@@ -38,9 +49,7 @@ function criptografar(texto) {
   } else {
     alert("Não é permitido caracteres especiais e letras maiúsculas!");
   }
-
   // retornar a versão criptografada
-
   return texto;
 }
 
@@ -52,14 +61,12 @@ function descriptografar(texto) {
   texto = texto.replace(/ober/g, "o");
   texto = texto.replace(/ufat/g, "u");
   texto = texto.replace(/mes/g, "");
-  
 
   if (checkChar(texto)) {
     console.log(texto);
   } else {
     alert("Não é permitido caracteres especiais e letras maiúsculas!");
   }
-
   // retornar a versão descriptografada
   return texto;
 }
